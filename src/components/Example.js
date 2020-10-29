79e281a565811bf0bcc1e2e28abae856faec38b1
Michael Baumer <meeesterbaumer@gmail.com>
2020-10-29 12:03:58 -0500

    Added single user

---
+++ src/components/Example.js
@@ -0,0 +1,12 @@
+import React from "react"
+import "./Example.css"
+import { User } from "./user/User"
+
+export const Example = () => (
+  <>
+    <div className="Title">Test</div>
+    <User />
+    <User />
+    <User />
+  </>
+)
---
+++ src/components/user/User.js
@@ -0,0 +1,10 @@
+import React from "react"
+import "./User.css"
+
+export const User = () => (
+  <div className="singleUserCont">
+    <div className="userFirstName">First Name: Dave</div>
+    <div className="userLastName">Last Name: Dobson</div>
+    <div className="userAddress">Address: 123 Any Street</div>
+  </div>
+)
